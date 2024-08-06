3f945172b3ec2bde2f2959594cd287db83f9192e
dishantg <dishantgupta1994@gmail.com>
2024-08-06 20:44:27 +0530

    - Initial commit - Grouping compound components

---
+++ src/components/Accordion/AccordionItem.jsx
@@ -0,0 +1,21 @@
+import { createContext, useContext } from "react";
+
+const AccordionItemContext = createContext();
+
+export function useAccordionItemContext() {
+  const ctx = useContext(AccordionItemContext);
+
+  if (!ctx) {
+    throw new Error("This component doesn't have AccordionItemContext");
+  }
+
+  return ctx;
+}
+
+export default function AccordionItem({ id, className, children }) {
+  return (
+    <AccordionItemContext.Provider value={id}>
+      <li className={className}>{children}</li>
+    </AccordionItemContext.Provider>
+  );
+}
